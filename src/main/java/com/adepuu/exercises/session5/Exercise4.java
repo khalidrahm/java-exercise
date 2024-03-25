53758a3c84ff091881517007342e9185bedf6785
unknown <khalidrahman2701@gmail.com>
2024-03-25 12:42:59 +0700

    Added for exercises4

--- src/main/java/com/adepuu/exercises/session5/Exercise4.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise4.java
@@ -1,5 +1,9 @@
 package com.adepuu.exercises.session5;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.ArrayList;
+
 public class Exercise4 {
     /**
      * Java Array Program to Remove Duplicate Elements From an Array
@@ -9,6 +13,22 @@ public class Exercise4 {
      * @param args
      */
     public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,3,2,1};
+        ArrayList<Integer> duplicated = removeDuplicate(arr);
+        System.out.println(duplicated);
 
+    }
+    public static ArrayList<Integer> removeDuplicate(int[] arra){
+        HashMap<Integer,Boolean> map = new HashMap<>();
+        ArrayList<Integer> uniqueArr = new ArrayList<>();
+        for(int i=0;i<arra.length;i++){
+            if (!map.containsKey(arra[i])){
+                map.put(arra[i], true);
+                uniqueArr.add(arra[i]);
+            }else{
+                map.put(arra[i], true);
+            }
+        }
+        return uniqueArr;
     }
 }
