e55695f3858e4b9172bc69f913370c8f1f33f9f6
unknown <khalidrahman2701@gmail.com>
2024-03-25 14:08:02 +0700

    Added for exercises8

--- src/main/java/com/adepuu/exercises/session5/Exercise8.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise8.java
@@ -1,5 +1,8 @@
 package com.adepuu.exercises.session5;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Exercise8 {
     /**
      * Java String Program to Find all Duplicates on Array
@@ -17,6 +20,22 @@ public class Exercise8 {
      * Output: []
      */
     public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,3,5,4};
+        ArrayList<Integer> duplicated = checkDuplicate(arr);
+        System.out.println(duplicated);
 
+    }
+    public static ArrayList<Integer> checkDuplicate(int[] arra){
+        HashMap<Integer,Boolean> map = new HashMap<>();
+        ArrayList<Integer> uniqueArr = new ArrayList<>();
+        for(int i=0;i<arra.length;i++){
+            if (!map.containsKey(arra[i])){
+                map.put(arra[i], true);
+            }else{
+                uniqueArr.add(arra[i]);
+                map.put(arra[i], false);
+            }
+        }
+        return uniqueArr;
     }
 }
