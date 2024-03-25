3d3b24bc143935de2c3f775e05353bb79f3def27
unknown <khalidrahman2701@gmail.com>
2024-03-25 18:52:29 +0700

    Added for exercises8

--- src/main/java/com/adepuu/exercises/session5/Exercise8.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise8.java
@@ -20,20 +20,22 @@ public class Exercise8 {
      * Output: []
      */
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,3,5,4};
+        int[] arr = {1,2,3,4,5,3,5,4,4,4,4,4};
         ArrayList<Integer> duplicated = checkDuplicate(arr);
         System.out.println(duplicated);
 
     }
     public static ArrayList<Integer> checkDuplicate(int[] arra){
         HashMap<Integer,Boolean> map = new HashMap<>();
+        HashMap<Integer,Boolean> duplicated = new HashMap<>();
         ArrayList<Integer> uniqueArr = new ArrayList<>();
         for(int i=0;i<arra.length;i++){
             if (!map.containsKey(arra[i])){
                 map.put(arra[i], true);
-            }else{
+            }else if(!duplicated.containsKey(arra[i])){
+                duplicated.put(arra[i], true);
                 uniqueArr.add(arra[i]);
-                map.put(arra[i], false);
+
             }
         }
         return uniqueArr;
