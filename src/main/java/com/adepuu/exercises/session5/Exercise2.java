7ee6303e9d62e5189d773fa3c83203d5d525802a
unknown <khalidrahman2701@gmail.com>
2024-03-25 11:49:55 +0700

    Added for exercises2

--- src/main/java/com/adepuu/exercises/session5/Exercise2.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise2.java
@@ -1,5 +1,5 @@
 package com.adepuu.exercises.session5;
-
+import java.util.Arrays;
 public class Exercise2 {
     /**
      * Java Array Program For Array Rotation
@@ -9,6 +9,17 @@ public class Exercise2 {
      * So, Final result: 3, 4, 5, 6, 7, 1, 2
      */
     public static void main(String[] args) {
-
+        int arr[] = {7,2,5,1,4};
+        int d = 2;
+        int[] rotated = rotate(arr,d);
+        System.out.println(Arrays.toString(rotated));
+    }
+    public static int[] rotate(int[] arra, int distance){
+        int [] rotatedArr = new int[arra.length];
+        for(int i = 0; i< arra.length;i++){
+            int newIndex = (i+arra.length-distance)%(arra.length);
+            rotatedArr[newIndex] = arra[i];
+        }
+        return rotatedArr;
     }
 }
