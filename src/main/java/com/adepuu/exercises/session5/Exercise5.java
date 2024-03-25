641545e98b98723c1931b49a35f72edcdd48ac8b
unknown <khalidrahman2701@gmail.com>
2024-03-25 13:17:38 +0700

    Added for exercises5

--- src/main/java/com/adepuu/exercises/session5/Exercise5.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise5.java
@@ -1,4 +1,7 @@
 package com.adepuu.exercises.session5;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Exercise5 {
     /**
@@ -13,6 +16,23 @@ public class Exercise5 {
      * Output: [ 8,7, 5, 2 ]
      */
     public static void main(String[] args) {
+        int[] arr = {8,7,5,2,3};
+        String direction = "asc";
+        ArrayList<Integer> sorted = sortArray(arr, direction);
+        System.out.println(sorted);
 
+    }
+    public static ArrayList<Integer> sortArray(int[] arr, String dir){
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i =0; i<arr.length;i++) {
+            list.add(arr[i]);
+        }
+        if (dir=="desc"){
+            Arrays.sort(arr);
+            Collections.sort(list, Collections.reverseOrder());
+        }else if(dir=="asc"){
+            Collections.sort(list);
+        }
+        return list;
     }
 }
